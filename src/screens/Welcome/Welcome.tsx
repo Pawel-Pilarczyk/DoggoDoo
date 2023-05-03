be96bdfd72dca9a16994c4196f29bd34c3bb4d75
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2023-05-03 20:32:24 +0200

    Scaling script

--- src/screens/Welcome/Welcome.tsx
+++ src/screens/Welcome/Welcome.tsx
@@ -1,12 +1,14 @@
 import { View } from 'react-native';
 import React, { FC } from 'react';
-import Typography from '@src/components/Typography';
+
+import Logo from '@src/assets/logo/DoggoDoo-logos_black.svg';
+
+import { styles } from './styles';
 
 const Welcome: FC = () => {
     return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Typography>Welcome Screen</Typography>
+        <View style={styles.wrapper}>
+            <Logo width={'240px'} height={'240px'} />
         </View>
     );
 };
---
+++ src/screens/Welcome/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+// import { scale, verticalScale } from '@src/utils/scale';
+
+export const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
